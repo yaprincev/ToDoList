377ae93850ae8192970f4a89cd205cfca1eefd8b
Yaprincev Maksim <5489812@mail.ru>
2024-11-15 16:52:17 +0300

    First init

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by student on 15.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
