bcd706662fc87209e7db549c8ac1a01ead605eca
Yaprincev Maksim <5489812@mail.ru>
2024-11-15 19:50:27 +0300

    EM-001 Make detail view with protocols

---
+++ ToDoList/ToDoList/DetailModule/View/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ToDoList
+//
+//  Created by student on 15.11.2024.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ ToDoList/ToDoList/DetailModule/View/DetailViewInput.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewInput.swift
+//  ToDoList
+//
+//  Created by student on 15.11.2024.
+//
+
+protocol DetailViewInput: AnyObject {
+}
