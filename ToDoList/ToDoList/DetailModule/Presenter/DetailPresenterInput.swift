d39e2bdf2f1a3d2fb086cc3bf1ea13a955865c34
Yaprincev Maksim <5489812@mail.ru>
2024-11-21 16:16:58 +0300

    EM-007 Configure detail screen

--- ToDoList/ToDoList/DetailModule/Presenter/DetailPresenterInput.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DetailPresenterInput.swift
-//  ToDoList
-//
-//  Created by student on 15.11.2024.
-//
-
-protocol DetailPresenterInput: AnyObject {
-}
--- ToDoList/ToDoList/DetailModule/Presenter/DetailPresenterOutput.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DetailPresenterOutput.swift
-//  ToDoList
-//
-//  Created by student on 15.11.2024.
-//
-
-protocol DetailPresenterOutput: AnyObject {
-}
